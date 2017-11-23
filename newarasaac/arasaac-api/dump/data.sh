9098f88463cea2e6f77ed54000533662aa2c7153
juanda <juandacorreo@gmail.com>
2017-11-23 18:04:40 +0100

    refactor code

--- newarasaac/arasaac-api/dump/data.sh
+++
@@ -1,2 +0,0 @@
- mongoimport --db arasaac --collection materials --file /docker-entrypoint-initdb.d/materials.json --jsonArray
- mongoimport --db arasaac --collection keywords --file  /docker-entrypoint-initdb.d/words.json  --jsonArray
