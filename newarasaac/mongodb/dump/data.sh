ca4cbd24761b8d3abd9882f216cbc87cfb49c807
juanda <juandacorreo@gmail.com>
2018-02-05 13:23:46 +0100

    add client and user for testing

--- newarasaac/mongodb/dump/data.sh
+++ newarasaac/mongodb/dump/data.sh
@@ -1,2 +1,4 @@
  mongoimport --db arasaac --collection materials --file /docker-entrypoint-initdb.d/materials.json --jsonArray
  mongoimport --db arasaac --collection keywords --file  /docker-entrypoint-initdb.d/words.json  --jsonArray
+ mongoimport --db arasaac --collection clients --file  /docker-entrypoint-initdb.d/clients.json  --jsonArray
+ mongoimport --db arasaac --collection users --file  /docker-entrypoint-initdb.d/users.json  --jsonArray
