5107e6f9306b337d27710cf1f863133ddf03f528
juanda <juandacorreo@gmail.com>
2018-02-26 14:55:18 +0100

    get scope from token

--- newarasaac/api/helpers/auth.js
+++ newarasaac/api/helpers/auth.js
@@ -3,7 +3,8 @@ import { authorization } from '../config'
 import { isArray } from 'util';
 const BearerStrategy = require('passport-http-bearer').Strategy
 const request = require('request')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
+import jwtDecode from 'jwt-decode'
 
 
 /**
@@ -20,28 +21,22 @@ passport.use(new BearerStrategy((accessToken, done) => {
   request.get(authUrl, (error, response/*, body*/)=>{
     if (error) done(null, false)
     else if (response.statusCode !== 200) {
-      console.log('NOT VALID!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+      console.log(response.body.error)
       done(null, false)
     }
-    console.log('**********************************************')
-    done(null, accessToken, { scopes: ['profile', 'atest'] })
+    else {
+      // get scope from token
+      const decoded = jwtDecode(accessToken);
+      done(null, accessToken, { scopes: decoded.scope })
+    }
   })
 }))
 
 
 
 module.exports = {
-  // curl -v -H "Authorization: Bearer 123456789" endpoint
-  // curl -v http://endpoint/?access_token=123456789  
   login:  (req, res, next) => {
-    // all scopes defined for login
-    // console.log(req.swagger.operation.securityDefinitions.login.scopes)
-
-    // requirement scope for this endpoint:
     const scopeRequired=req.swagger.operation.security[0].login
-    console.log(`scopeRequired: ${scopeRequired}`)
-
-    
     passport.authenticate('bearer', { session: false }, (err, user, info) => {
       if (err) return res.status(500).send(`Error`)
       if (!user) return res.status(401).send(`Unauthorized!`)
@@ -55,63 +50,4 @@ module.exports = {
     })(req, res, next)
     
   } 
-
-/*
-    {
-    var currentScopes = req.swagger.operation['x-security-scopes']
-    console.log (req)
-    const sendError = () => res.status(403).json({ message: 'Error: Access Denied' })
-
-    /*
-    //validate the 'Authorization' header. it should have the following format:
-    //'Bearer tokenString'
-    if (token && token.indexOf("Bearer ") === 0) {
-      var tokenString = token.split(" ")[1];
-
-      jwt.verify(tokenString, sharedSecret, function (
-        verificationError,
-        decodedToken
-      ) {
-        //check if the JWT was verified correctly
-        if (
-          verificationError == null &&
-          Array.isArray(currentScopes) &&
-          decodedToken &&
-          decodedToken.role
-        ) {
-          // check if the role is valid for this endpoint
-          var roleMatch = currentScopes.indexOf(decodedToken.role) !== -1;
-          // check if the issuer matches
-          var issuerMatch = decodedToken.iss == issuer;
-
-          // you can add more verification checks for the
-          // token here if necessary, such as checking if
-          // the username belongs to an active user
-
-          if (roleMatch && issuerMatch) {
-            //add the token to the request so that we
-            //can access it in the endpoint code if necessary
-            req.auth = decodedToken;
-            //if there is no error, just return null in the callback
-            return callback(null);
-          } else {
-            //return the error in the callback if there is one
-            return callback(sendError());
-          }
-        } else {
-          //return the error in the callback if the JWT was not verified
-          return callback(sendError());
-        }
-      });
-    } else {
-      //return the error in the callback if the Authorization header doesn't have the correct format
-      return callback(sendError());
-    }
-
-    if (req.headers.authorization === 'Scott') {
-      return next()
-    }
-    return next(new Error('access denied!'))
-  }]
-  */
 }
