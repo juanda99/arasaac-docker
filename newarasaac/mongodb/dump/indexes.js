8374a3f0d1f64f32b11d5515532159254592bf59
juanda <juandacorreo@gmail.com>
2018-05-25 08:28:41 +0200

    indexes for pictos

--- newarasaac/mongodb/dump/indexes.js
+++ newarasaac/mongodb/dump/indexes.js
@@ -1,13 +1,5 @@
 db = db.getSiblingDB('arasaac')
-db.materials.createIndex(
-  { "title": "text", "desc": "text", "translations.title": "text", "translations.desc": "text"},
-  {
-    "weights":
-      { "title": 10, "desc":1, "translations.title": 10, "translations.desc": 1 },
-    "default_language": "spanish"
-  }
-);
-const languages = ['es', 'ar'];
+const languages = ['ar', 'bg', 'br', 'ca', 'cr', 'de', 'en', 'es', 'eu', 'fr', 'ga', 'it', 'pl', 'pt', 'ro', 'ru', 'val', 'zh'];
 languages.forEach(language => {
   const collectionName = `pictos_${language}`
   db[collectionName].createIndex(
@@ -15,21 +7,7 @@ languages.forEach(language => {
     {
       "weights":
         { "keywords.keyword": 10, "tags":1 },
-      "default_language": language
+      "default_language": "None"
     }
   );
 });
-db.pictograms.createIndex(
-  { "keywords.keyword": "text", "tags": "text"},
-  {
-    "weights":
-      { "keywords.keyword": 10, "tags":1 },
-    "default_language": "spanish"
-  }
-);
-db.tests.createIndex(
-  { "word": "text"},
-  {
-    "default_language": "spanish"
-  }
-);
