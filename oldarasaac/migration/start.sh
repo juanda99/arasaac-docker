a722297b196027a60a51e867432db56c4aa2ecfb
juanda <juandacorreo@gmail.com>
2018-12-18 01:15:49 +0100

    add nl

--- oldarasaac/migration/start.sh
+++ oldarasaac/migration/start.sh
@@ -3,6 +3,6 @@ echo "***********START MIGRATION***************"
 echo "Generating pictos...."
 python procesar_sql_a_mongo_pymongo.py
 #echo "Generating words.py...."
-#python words.py
+python words.py
 #echo "Generating materials..."
-#python materials.py
+python materials.py
