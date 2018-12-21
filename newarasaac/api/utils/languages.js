5a10b3822f9c30c6d246c9c39bd448ff377f572e
juanda <juandacorreo@gmail.com>
2018-12-21 11:05:51 +0100

    refactor and and identities for pictograms

---
+++ newarasaac/api/utils/languages.js
@@ -0,0 +1,22 @@
+const languages = [
+  'es',
+  'ru',
+  'ro',
+  'ara', // instead of ar
+  'zhs', // instead of zh, zhs simplified chineese zht traditional chineese
+  'bg', // not available
+  'pl', // not available
+  'en',
+  'fr',
+  'ca', // not available
+  'eu', // not available
+  'de',
+  'it',
+  'pt',
+  'ga', // not available
+  'br', // not available, should we use pt?
+  'cr', // not available
+  'val' // not available
+]
+
+module.exports = languages
