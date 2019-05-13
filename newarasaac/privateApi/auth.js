0d928bc760b243f5e5deea6e1c8f68fba071e25f
juanda <juandacorreo@gmail.com>
2019-05-13 14:55:06 +0200

    add debug traces

--- newarasaac/privateApi/auth.js
+++ newarasaac/privateApi/auth.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const passport = require('passport')
 const Strategy = require('passport-http-bearer').Strategy
 const jwt = require('jsonwebtoken')
+const logger = require('./utils/logger')
 
 // see https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253 for axios catch errors
 
@@ -13,11 +14,17 @@ passport.use(
   new Strategy(async (token, cb) => {
     const url = `http://auth/api/tokeninfo?access_token=${token}`
     try {
+      if (!token) {
+        logger.debug('Authentication failed: No token!!')
+        return cb(null, false)
+      }
       await axios.get(url) // no error (20X code), go on
       const { iss, sub, aud, role, exp, scope } = jwt.decode(token)
       const user = { user: sub, role, scope, iss, aud, exp }
+      logger.debug(`Authentication ok for user ${sub}`)
       return cb(null, user)
     } catch (error) {
+      logger.debug(`Authentication failed ${error.message}`)
       if (error.response) {
         const { status } = error.response
         // const { data, status, headers } = error.response
