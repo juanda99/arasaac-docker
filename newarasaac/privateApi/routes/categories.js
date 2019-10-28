9f740f592e95eab0c04c2b3a9d7862f9423eb43c
juanda <juandacorreo@gmail.com>
2019-10-28 09:14:42 +0100

    add auth for categories crud

--- newarasaac/privateApi/routes/categories.js
+++ newarasaac/privateApi/routes/categories.js
@@ -1,19 +1,36 @@
 const router = require('express').Router()
+const passport = require('passport')
+const { hasRole } = require('../middlewares')
 const categoriesController = require('../controllers/categoriesController')
 
 router.get('/:locale/:lastUpdated?', (req, res) => {
   categoriesController.get(req, res)
 })
 
-router.put('/', (req, res) => {
-  categoriesController.update(req, res)
-})
+router.put(
+  '/',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('translator'),
+  (req, res) => {
+    categoriesController.update(req, res)
+  }
+)
 
-router.put('/add', (req, res) => {
-  categoriesController.add(req, res)
-})
-router.put('/remove', (req, res) => {
-  categoriesController.remove(req, res)
-})
+router.put(
+  '/add',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('admin'),
+  (req, res) => {
+    categoriesController.add(req, res)
+  }
+)
+router.put(
+  '/remove',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('admin'),
+  (req, res) => {
+    categoriesController.remove(req, res)
+  }
+)
 
 module.exports = router
