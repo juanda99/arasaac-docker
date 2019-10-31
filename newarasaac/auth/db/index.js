6475fa63fd5b5dc284217ad73a5d891727c50c88
juanda <juandacorreo@gmail.com>
2019-10-31 14:11:30 +0100

    fix add authentication

--- newarasaac/auth/db/index.js
+++ newarasaac/auth/db/index.js
@@ -1,19 +1,29 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
-const databaseUrl = process.env.MONGO_URL || 'mongodb://mongodb/arasaac';
+const mongoose = require("mongoose");
+const MONGO_DB_USER = process.env.MONGO_DB_USER;
+const MONGO_DB_PWD = process.env.MONGO_DB_PWD;
+const databaseUrl = `mongodb://${MONGO_DB_USER}:${MONGO_DB_PWD}@mongodb/arasaac?authSource=admin`;
 mongoose.connect(databaseUrl);
-mongoose.connection.on('connected', () => console.log('Connected to database: ' + databaseUrl))
-mongoose.connection.on('error',(err) => console.log('Database connection error: ' + err))
-mongoose.connection.on('disconnected', () => console.log('Disconnected from database'))
+mongoose.connection.on("connected", () =>
+  console.log("Connected to database: " + databaseUrl)
+);
+mongoose.connection.on("error", err =>
+  console.log("Database connection error: " + err)
+);
+mongoose.connection.on("disconnected", () =>
+  console.log("Disconnected from database")
+);
 
-process.on('SIGINT', () => mongoose.connection.close( () => {
-  console.log('Finished App and disconnected from database')
-  process.exit(0)
-}))
+process.on("SIGINT", () =>
+  mongoose.connection.close(() => {
+    console.log("Finished App and disconnected from database");
+    process.exit(0);
+  })
+);
 
-exports.accessTokens       = require('./accesstokens');
-exports.authorizationCodes = require('./authorizationcodes');
-exports.clients            = require('./clients');
-exports.refreshTokens      = require('./refreshtokens');
-exports.users              = require('./users');
+exports.accessTokens = require("./accesstokens");
+exports.authorizationCodes = require("./authorizationcodes");
+exports.clients = require("./clients");
+exports.refreshTokens = require("./refreshtokens");
+exports.users = require("./users");
