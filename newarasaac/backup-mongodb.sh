767848582b3d2dd0a1857921c73a24eb902fe407
juanda <juandacorreo@gmail.com>
2019-11-14 12:18:56 +0100

    add backup script for mongodb

---
+++ newarasaac/backup-mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+source .env
+export CONTAINER_NAME="mongodb"
+export DATABASE_NAME="arasaac"
+
+export TIMESTAMP=$(date +'%Y%m%d%H%M%S')
+
+docker exec -t ${CONTAINER_NAME} mongodump --out /data/backup/${DATABASE_NAME}-${TIMESTAMP} --username ${MONGO_DB_USER} --password ${MONGO_DB_PWD} --authenticationDatabase admin --db ${DATABASE_NAME}
