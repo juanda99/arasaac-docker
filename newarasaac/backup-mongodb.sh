01e3ccc9393bb948bfda0054f3aeec36c9850e7e
juanda <juandacorreo@gmail.com>
2019-11-14 18:29:25 +0100

    modify backup to a single file

--- newarasaac/backup-mongodb.sh
+++ newarasaac/backup-mongodb.sh
@@ -3,6 +3,6 @@ source .env
 export CONTAINER_NAME="mongodb"
 export DATABASE_NAME="arasaac"
 
-export TIMESTAMP=$(date +'%Y%m%d%H%M%S')
+export TIMESTAMP=$(date +'%d%m%Y_%H:%M')
 
-docker exec -t ${CONTAINER_NAME} mongodump --out /data/backup/${DATABASE_NAME}-${TIMESTAMP} --username ${MONGO_DB_USER} --password ${MONGO_DB_PWD} --authenticationDatabase admin --db ${DATABASE_NAME}
+docker exec -t ${CONTAINER_NAME} mongodump --archive=/data/backup/${DATABASE_NAME}-${TIMESTAMP}.archive --username ${MONGO_DB_USER} --password ${MONGO_DB_PWD} --authenticationDatabase admin --db ${DATABASE_NAME}
