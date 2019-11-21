e897ad200f1238148289984719b7f63f65f75206
juanda <juandacorreo@gmail.com>
2019-11-21 15:41:26 +0100

    add modify event

--- newarasaac/svgwatcher/index.js
+++ newarasaac/svgwatcher/index.js
@@ -1,33 +1,33 @@
 // load dependencies
 
-const chokidar = require('chokidar')
-const path = require('path')
-const logger = require('./logger')
-const createPNGFiles = require('./createPNGFiles')
-const { preCompiledOptions } = require('./utils/svg')
+const chokidar = require("chokidar");
+const path = require("path");
+const logger = require("./logger");
+const createPNGFiles = require("./createPNGFiles");
+const { preCompiledOptions } = require("./utils/svg");
 
 // global
-require('dotenv').config()
+require("dotenv").config();
 
-const INCLUDE_FILE = 'include'
-const EXCLUDE_FILE = 'exclude'
-const SVG_DIR = process.env.SVG_DIR || '/app/svg'
-const usePolling = parseInt(process.env.CHOKIDAR_USEPOLLING) || 1
-const RESOLUTIONS = [300, 500, 2500]
+const INCLUDE_FILE = "include";
+const EXCLUDE_FILE = "exclude";
+const SVG_DIR = process.env.SVG_DIR || "/app/svg";
+const usePolling = parseInt(process.env.CHOKIDAR_USEPOLLING) || 1;
+const RESOLUTIONS = [300, 500, 2500];
 
 // startup logs
-logger.info('ARASAAC SVG-WATCHER STARTED')
-logger.info(`Using folder: ${SVG_DIR}`)
-logger.info(`Using resolutions: ${RESOLUTIONS}`)
+logger.info("ARASAAC SVG-WATCHER STARTED");
+logger.info(`Using folder: ${SVG_DIR}`);
+logger.info(`Using resolutions: ${RESOLUTIONS}`);
 RESOLUTIONS.forEach(resolution => {
   logger.info(
     `Image options for ${resolution}px: ${JSON.stringify(
       preCompiledOptions[resolution]
     )}`
-  )
-})
+  );
+});
 
-logger.info(`Start scanning....`)
+logger.info(`Start scanning....`);
 
 // Init watcher
 const watcher = chokidar.watch(`${SVG_DIR}/*.svg`, {
@@ -37,29 +37,33 @@ const watcher = chokidar.watch(`${SVG_DIR}/*.svg`, {
   cwd: SVG_DIR,
   awaitWriteFinish: {
     stabilityThreshold: 3000,
-    pollInterval: 300
+    pollInterval: 50
   }
-})
+});
 
 // Add event listeners.
 watcher
-  .on('add', file => {
-    logger.info(`WATCHER - ADD FILE: ${path.resolve(SVG_DIR, file)}`)
-    addTask(file, INCLUDE_FILE)
+  .on("add", file => {
+    logger.info(`WATCHER - ADD FILE: ${path.resolve(SVG_DIR, file)}`);
+    addTask(file, INCLUDE_FILE);
+  })
+  .on("change", file => {
+    logger.info(`MODIFY FILE: ${path.resolve(SVG_DIR, file)}`);
+    addTask(file, INCLUDE_FILE);
   })
 
-  .on('unlink', file => {
+  .on("unlink", file => {
     // generate new zip or remove screenshot
-    logger.info(`WATCHER - REMOVED FILE: ${path.resolve(SVG_DIR, file)}`)
-    addTask(file, EXCLUDE_FILE)
+    logger.info(`WATCHER - REMOVED FILE: ${path.resolve(SVG_DIR, file)}`);
+    addTask(file, EXCLUDE_FILE);
   })
-  .on('error', error => logger.error(`WATCHER ERROR: ${error.message}`))
-  .on('ready', () => logger.info('Initial scan complete, waiting for changes'))
+  .on("error", error => logger.error(`WATCHER ERROR: ${error.message}`))
+  .on("ready", () => logger.info("Initial scan complete, waiting for changes"));
 
 const addTask = (file, operation) => {
   if (operation === INCLUDE_FILE) {
-    logger.info(`ADD FILE : ${file}`)
+    logger.info(`ADD FILE : ${file}`);
     // resize just when size is bigger than 1500
-    RESOLUTIONS.forEach(resolution => createPNGFiles(file, resolution))
+    RESOLUTIONS.forEach(resolution => createPNGFiles(file, resolution));
   }
-}
+};
