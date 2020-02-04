723641c954761a80d60c5d509a788ecaa1b31775
juanda <juandacorreo@gmail.com>
2020-02-04 14:26:09 +0100

    new version watcher.js

--- newarasaac/watcher/watcher.js
+++ newarasaac/watcher/watcher.js
@@ -18,6 +18,7 @@ var localePattern = /^[A-z]{2,3}$/g;
 const INCLUDE_FILE = "include";
 const EXCLUDE_FILE = "exclude";
 const INCLUDE_DIR = "includedir";
+const EXCLUDE_DIR = "excludedir";
 const SCREENSHOTS_DIR = process.env.SCREENSHOTS_DIR || "screenshots";
 const MATERIALS = process.env.MATERIALS || "/app/materials";
 const RESOLUTION = process.env.RESOLUTION || 300;
@@ -59,40 +60,175 @@ watcher
 
   .on("addDir", dir => {
     // only task is to push dir into targetLanguages field if it's a language dir
-    logger.info(`ADDED DIRECTORY: ${path.resolve(MATERIALS, dir)}`);
+    logger.info(`WATCHER - ADDED DIRECTORY: ${path.resolve(MATERIALS, dir)}`);
     addTask(dir, INCLUDE_DIR);
   })
+
   .on("change", file => {
     // generate new zip or resize screenshot (same as add task)
     logger.info(`WATCHER - CHANGED FILE: ${path.resolve(MATERIALS, file)}`);
     addTask(file, INCLUDE_FILE);
   })
+
+  .on("unlinkDir", dir => {
+    logger.info(`WATCHER - REMOVED DIRECTORY: ${path.resolve(MATERIALS, dir)}`);
+    addTask(dir, EXCLUDE_DIR);
+  })
+
   .on("unlink", file => {
     // generate new zip or remove screenshot
     logger.info(`WATCHER - REMOVED FILE: ${path.resolve(MATERIALS, file)}`);
     addTask(file, EXCLUDE_FILE);
   })
+
   .on("error", error => logger.error(`WATCHER ERROR: ${error}`))
   .on("ready", () => logger.info("Initial scan complete, waiting for changes"));
 
-const initMaterial = materialId => {
-  let languages = dirs(path.resolve(MATERIALS, materialId));
-  materialFiles[materialId] = {
-    materialId,
-    includeScreenshots: [],
-    excludeScreenshots: [],
-    languages: new Set(languages),
-    targetLanguages: new Set()
-  };
+
+
+const addTask = (file, operation) => {
+
+  switch (operation) {
+    case INCLUDE_DIR:
+      {
+        const materialId = !isNaN(file) ? file : path.dirname(file).split(path.sep)[0];
+        // check if dir is materialId
+        if (materialId === file) {
+          logger.info(`ADD NEW MATERIAL ${materialId}`);
+        } else {
+          // check if file is a language forlder
+          const relativeDir = path.basename(file)
+          if (file.match(/^[0-9]+\/[A-z]{2,3}$/)) {
+            logger.info(`ADD LANGUAGE: ${relativeDir} for material ${materialId}`);
+            // check if file is related to screenshots folders
+          } else if ((file.match(/^[0-9]+\/screenshots$/)) || (file.match(/^[0-9]+\/screenshots\/[A-z]{2,3}$/))) {
+            logger.info(`Screenshots directory ${file} added, but not executing any action, waiting for screenshots!`);
+            // directories for lim, jclic.... should put also zip file to take it into account
+          } else {
+            logger.info(`Directory ${file} added, but not executing any action!`);
+          }
+        }
+      }
+      break;
+    case EXCLUDE_DIR:
+      {
+        const materialId = !isNaN(file) ? file : path.dirname(file).split(path.sep)[0];
+        // check if dir is materialId
+        if (materialId === file) {
+          logger.info(`REMOVE MATERIAL ${materialId}`);
+        } else {
+          // check if file is a language folder
+          const relativeDir = path.basename(file)
+          if (file.match(/^[0-9]+\/[A-z]{2,3}$/)) {
+            logger.info(`REMOVE LANGUAGE: ${relativeDir} from material ${materialId}, generating new material zip files...`);
+            debouncedSync(materialId);
+            // check if file is related to screenshots folders
+          } else if ((file.match(/^[0-9]+\/screenshots$/)) || (file.match(/^[0-9]+\/screenshots\/[A-z]{2,3}$/))) {
+            const dirToRemove = file.replace("screenshots", `screenshots_${RESOLUTION}`);
+            // delete the file from screenshots)
+            fs.remove(path.resolve(MATERIALS, dirToRemove), err => {
+              if (err) logger.error(`ERROR REMOVE ${dirToRemove}: ${err}`);
+              else logger.info(`REMOVED DIRECTORY: ${dirToRemove}`);
+            });
+            return
+            // directories for lim, jclic.... should put also zip file to take it into account
+          } else {
+            logger.info(`Directory ${file} removed, but not executing any action!`);
+          }
+        }
+      }
+      break;
+    case INCLUDE_FILE:
+      {
+        const materialId = !isNaN(file) ? file : path.dirname(file).split(path.sep)[0];
+        const dir = path.dirname(file);
+        // check if file is inside materialId or inside a language forlder
+        if (dir.match(/^[0-9]+$/) || dir.match(/^[0-9]+\/[A-z]{2,3}$/)) {
+          logger.info(`ADD FILE: ${file}, generating new material zip files...`);
+          debouncedSync(materialId);
+          // check if file is related to screenshots folders
+        } else if ((dir.match(/^[0-9]+\/screenshots$/)) || (dir.match(/^[0-9]+\/screenshots\/[A-z]{2,3}$/))) {
+          resizeImage(file, RESOLUTION)
+          // directories for lim, jclic.... should put also zip file to take it into account
+        } else {
+          logger.info(`File ${file} added, but not executing any action!`);
+        }
+      }
+      break;
+    case EXCLUDE_FILE:
+      {
+        const materialId = !isNaN(file) ? file : path.dirname(file).split(path.sep)[0];
+        const dir = path.dirname(file);
+        // check if file is inside materialId or inside a language forlder
+        if (dir.match(/^[0-9]+$/) || dir.match(/^[0-9]+\/[A-z]{2,3}$/)) {
+          logger.info(`REMOVE FILE: ${file}, generating new material zip file...`);
+          debouncedSync(materialId);
+          // check if file is related to screenshots folders
+        } else if ((dir.match(/^[0-9]+\/screenshots$/)) || (dir.match(/^[0-9]+\/screenshots\/[A-z]{2,3}$/))) {
+          const fileToRemove = file.replace("screenshots", `screenshots_${RESOLUTION}`);
+          fs.remove(path.resolve(MATERIALS, fileToRemove), err => {
+            if (err) logger.error(`ERROR REMOVE ${fileToRemove}: ${err}`);
+            else logger.info(`REMOVED FILE: ${fileToRemove}`);
+          });
+          // directories for lim, jclic.... should put also zip file to take it into account
+        } else {
+          logger.info(`File ${file} removed, but not executing any action!`);
+        }
+      }
+      break;
+    default:
+      logger.info(`File/dir ${file} ${operation}, NOT EXECUTING ANY ACTION!!!!!!!`);
+  }
 };
 
+const debouncedSync = _.debounce(
+  async idMaterial => {
+    logger.info(`GENERATING zip files for material ${idMaterial}...`)
+    try {
+      // get previous files and remove all of them:
+      const zipFiles = await getZipFiles(idMaterial);
+      await Promise.all(zipFiles.map((zipFile) => fs.remove(path.resolve(MATERIALS, idMaterial, zipFile))))
+
+      // get all languages to generate new zip files:
+      const languages = getLanguagesFromDir(path.resolve(MATERIALS, idMaterial))
+      if (languages.length === 0) languages.push('xx')
+      // case no language defined, we take a default one, called 'xx'
+      languages.forEach(async language => {
+        const newZip = path.resolve(
+          MATERIALS,
+          idMaterial,
+          `index-${language}-${uuidv4()}.tgz`
+        );
+        let files = listFiles(path.resolve(MATERIALS, idMaterial));
+        let copyFiles = language == "xx" ? [...files] : [...files, language];
+        // if language doesn't exist (xx case) it throws an error
+        tar.c(
+          {
+            gzip: true,
+            sync: true,
+            onwarn: prueba,
+            file: newZip,
+            cwd: path.resolve(MATERIALS, idMaterial)
+          },
+          copyFiles
+        );
+        logger.info(`ZIP GENERATED: ${newZip}`);
+      });
+    } catch (err) {
+      logger.error(`Error executing debouncedSync for material ${idMaterial}: ${err}`);
+    }
+  }, (wait = 10000)
+)
+
 // function to get all languages from a directory
-const dirs = p =>
+const getLanguagesFromDir = p =>
   fs
     .readdirSync(p)
     .filter(
       f => fs.statSync(path.join(p, f)).isDirectory() && f.match(/^[A-z]{2,3}$/)
     );
+
+
 // list files, not directories, not tgz.
 const listFiles = p =>
   fs.readdirSync(p).filter(f => {
@@ -103,136 +239,23 @@ const listFiles = p =>
     return !fs.statSync(path.join(p, f)).isDirectory() && !filePattern.test(f);
   });
 
-const addTask = (file, operation) => {
-  let materialId = path.dirname(file).split(path.sep)[0];
-  let dir = path.dirname(path.resolve(MATERIALS, file));
-  let relativeDir = path.basename(file);
-  // if not initialized, we do it before pushing data
-  if (!materialFiles[materialId]) initMaterial(materialId);
-  let material = materialFiles[materialId];
-  // depending on material type we add where it should be
-  // *screenshots*
-  if (dir.match(/screenshots$/) || dir.match(/screenshots\/[A-z]{2,3}$/))
-    material[`${operation}Screenshots`].push(file);
-  // *directories*
-  else if (operation == INCLUDE_DIR && relativeDir.match(/^[A-z]{2,3}$/)) {
-    material.languages.add(relativeDir);
-    logger.info(`ADD LANGUAGE: ${relativeDir} for material ${materialId}`);
-  } else if (
-    operation == INCLUDE_DIR &&
-    !path.basename(file).match(/^[A-z]{2,3}$/)
-  ) {
-    logger.warn(`Directory ${file} added, but not executing any action!`);
-    return;
-    // *material files*
-  } else {
-    let dirName = dir.split(path.sep).pop();
-    let parentDir = path.resolve(dir, "..");
-    let parentDirName = parentDir.split(path.sep).pop();
-    if (dirName == materialId) material.targetLanguages = material.languages;
-    else if (parentDirName == materialId) material.targetLanguages.add(dirName);
-    //dirName should be the language
-    else {
-      logger.warn(`File ${file} added, but not executing any action!`);
-      return;
-    }
-  }
-  // call our actions via sync - debounce (lodash)
-  operateDebounced(material);
-};
-
-// https://stackoverflow.com/questions/28787436/debounce-a-function-with-argument
-var operateDebounced = _.wrap(
-  _.memoize(() => _.debounce(sync, (wait = TIME)), _.property("materialId")),
-  (func, obj) => func(obj)(obj)
-);
-
-const sync = async material => {
-  // remove files from screenshosts_300
-  material.excludeScreenshots.map(file => {
-    file.replace("screenshots", `screenshots_${RESOLUTION}`);
-    // delete the file from screenshots)
-    fs.remove(path.resolve(MATERIALS, file), err => {
-      if (err) logger.error(err);
-      else logger.info(`REMOVE SCREENSHOT: ${file}`);
-    });
-  });
-  // add screenshots to screenshots_300
-  material.includeScreenshots.map(file =>
-    resizeImage(file, material.materialId, RESOLUTION)
-  );
-
-  // zip files
-  try {
-    zipFiles(material);
-    // if (material.targetLanguages.size) zipFiles(material)
-  } catch (err) {
-    logger.error(err);
-  }
-
-  // reset materialId
-  material.includeScreenshots = [];
-  material.excludeScreenshots = [];
-  material.targetLanguages = new Set();
-};
-
-const zipFiles = async material => {
-  // get all the ZipFiles per language
-  let id = material.materialId;
-  try {
-    let zipFiles = getZipFiles(id);
-    // case no language defined, we take a default one, called 'xx'
-    if (material.targetLanguages.size === 0) material.targetLanguages.add("xx");
-    material.targetLanguages.forEach(async language => {
-      let languageZipFiles = getZipFile(language, zipFiles);
-      languageZipFiles.forEach(
-        async file => await fs.remove(path.resolve(MATERIALS, id, file))
-      );
-      let newZip = path.resolve(
-        MATERIALS,
-        id,
-        `index-${language}-${uuidv4()}.tgz`
-      );
-      let files = listFiles(path.resolve(MATERIALS, id));
-      let copyFiles = language == "xx" ? [...files] : [...files, language];
-      // if language doesn't exist (xx case) it throws an error
-      tar.c(
-        {
-          gzip: true,
-          sync: true,
-          onwarn: prueba,
-          file: newZip,
-          cwd: path.resolve(MATERIALS, id)
-        },
-        copyFiles
-      );
-      logger.info(`ZIP GENERATED: ${newZip}`);
-    });
-  } catch (error) {
-    logger.error(error);
-  }
-};
 const prueba = (message, data) => {
   console.log(message);
 };
 
-const resizeImage = (file, materialId, size) => {
-  let extension = path.extname(file).toLowerCase();
+const resizeImage = (file, size) => {
+  const extension = path.extname(file).toLowerCase();
   if (
     extension === ".png" ||
     extension === ".jpg" ||
     extension === ".jpeg" ||
     extension === ".gif"
   ) {
-    let newDir = path.resolve(
-      MATERIALS,
-      path.dirname(file),
-      "..",
-      `${SCREENSHOTS_DIR}_${size}`
-    );
+    const dir = path.resolve(MATERIALS, path.dirname(file));
+    const newDir = dir.replace("screenshots", `screenshots_${size}`);
     fs.ensureDir(newDir)
       .then(() => {
-        let fileName = path.basename(file);
+        const fileName = path.basename(file);
         sharp(`${MATERIALS}/${file}`)
           .resize(null, parseInt(size))
           .toFile(`${newDir}/${fileName}`, function (err) {
@@ -241,28 +264,29 @@ const resizeImage = (file, materialId, size) => {
           });
       })
       .catch(err => {
-        logger.error(`Error creating dir for screenshots:${err}`);
+        logger.error(`Error creating dir for screenshots: ${err}`);
       });
   }
 };
 
 const getZipFiles = materialId => {
-  let filePattern = new RegExp(
+  const filePattern = new RegExp(
     `^index-[A-z]{2,3}-[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}.tgz$`,
     "i"
   );
-  let files = fs.readdirSync(path.resolve(MATERIALS, materialId));
+  const files = fs.readdirSync(path.resolve(MATERIALS, materialId));
   return files.filter(file => filePattern.test(file));
 };
 
 const getZipFile = (language, zipFiles) => {
   try {
-    let filePattern = new RegExp(
+    const filePattern = new RegExp(
       `^index-${language}-[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}.tgz$`,
       "i"
     );
     return zipFiles.filter(file => filePattern.test(file));
   } catch (error) {
-    logger.error(error);
+    logger.error(`getZipFile ${error.message}`);
   }
 };
+
