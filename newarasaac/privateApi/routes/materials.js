1085785179d6000dad479e13e5c12653ca027e11
juanda <juandacorreo@gmail.com>
2020-02-08 11:52:30 +0100

    need admin role

--- newarasaac/privateApi/routes/materials.js
+++ newarasaac/privateApi/routes/materials.js
@@ -1,13 +1,27 @@
 const router = require('express').Router()
 const materialsController = require('../controllers/materialsController')
+const passport = require('passport')
+const { hasRole } = require('../middlewares')
 
-router.post('/', (req, res) => {
-  materialsController.create(req, res)
-})
-router.put('/:id', (req, res) => {
-  materialsController.update(req, res)
-})
-router.delete('/:id', (req, res) => {
-  materialsController.delete(req, res)
-})
+router.post('/',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('admin'),
+  (req, res) => {
+    materialsController.create(req, res)
+  })
+
+router.put('/:id',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('admin'),
+  (req, res) => {
+    materialsController.update(req, res)
+  })
+
+router.delete('/:id',
+  passport.authenticate('bearer', { session: false }),
+  hasRole('admin'),
+  (req, res) => {
+    materialsController.remove(req, res)
+  })
+
 module.exports = router
