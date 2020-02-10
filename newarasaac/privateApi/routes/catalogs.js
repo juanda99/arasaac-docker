5d7a9d429cc9e9fda4802a0a74ccc38c0dad2005
juanda <juandacorreo@gmail.com>
2020-02-10 23:06:42 +0100

    need to be admin for catalog issues

--- newarasaac/privateApi/routes/catalogs.js
+++ newarasaac/privateApi/routes/catalogs.js
@@ -1,17 +1,37 @@
 const router = require('express').Router()
 const catalogsController = require('../controllers/catalogsController')
 
+const passport = require('passport')
+const { hasRole } = require('../middlewares')
+
 const returnRouter = io => {
-  router.post('/', (req, res) =>
-    catalogsController.createAllCatalogs(req, res, io)
-  )
-  router.post('/:locale', (req, res) =>
-    catalogsController.createCatalogByLanguage(req, res, io)
-  )
-  router.get('/', (req, res) => catalogsController.getAllCatalogs(req, res))
-  router.get('/:locale', (req, res) =>
-    catalogsController.getCatalogsByLanguage(req, res)
-  )
+  router.post('/',
+    passport.authenticate('bearer', { session: false }),
+    hasRole('admin'),
+    (req, res) => {
+      catalogsController.createAllCatalogs(req, res, io)
+    })
+
+  router.post('/:locale',
+    passport.authenticate('bearer', { session: false }),
+    hasRole('admin'),
+    (req, res) => {
+      catalogsController.createCatalogByLanguage(req, res, io)
+    })
+
+  router.get('/',
+    passport.authenticate('bearer', { session: false }),
+    hasRole('admin'),
+    (req, res) => {
+      catalogsController.getAllCatalogs(req, res)
+    })
+
+  router.get('/:locale',
+    passport.authenticate('bearer', { session: false }),
+    hasRole('admin'),
+    (req, res) => {
+      catalogsController.getCatalogsByLanguage(req, res)
+    })
   return router
 }
 
