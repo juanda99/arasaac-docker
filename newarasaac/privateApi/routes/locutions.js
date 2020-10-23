735292933daef96e9894af9a47e478d0c9d168d1
juanda <juandacorreo@gmail.com>
2020-10-23 13:00:04 +0200

    fix route for locutions download

--- newarasaac/privateApi/routes/locutions.js
+++ newarasaac/privateApi/routes/locutions.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
 const locutionsController = require('../controllers/locutionsController')
 
-router.get('/:id/:locale/:keyword', (req, res) => {
-  locutionsController.getLocutionById(req, res)
-})
+router.get('/:locale/:keyword', (req, res) => locutionsController.getLocutionById(req, res))
 
 module.exports = router
